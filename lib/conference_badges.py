1a22c163066b12f7533299cfee8b574496fa129c
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-08 10:31:21 -0600

    finished using for loops

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,23 @@
 def badge_maker(name):
-    return None
+    return (f'Hello, my name is {name}.')
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badges.append(badge_maker(name))
+    return badges
 
 def assign_rooms(names):
-    return None
+    rooms = []
+    for index, name in enumerate(names):
+        rooms.append(f'Hello, {name}! You\'ll be assigned to room {index + 1}!')
+    return rooms
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    rooms = assign_rooms(names)
+
+    for badge in badges:
+        print(badge)
+    for room in rooms:
+        print(room)
