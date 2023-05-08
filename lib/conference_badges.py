988a0573dfa916908014cf1493bdf3e306f5407e
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-08 10:34:13 -0600

    refactored to list comprehensions

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -2,15 +2,11 @@ def badge_maker(name):
     return (f'Hello, my name is {name}.')
 
 def batch_badge_creator(names):
-    badges = []
-    for name in names:
-        badges.append(badge_maker(name))
+    badges = [badge_maker(name) for name in names]
     return badges
 
 def assign_rooms(names):
-    rooms = []
-    for index, name in enumerate(names):
-        rooms.append(f'Hello, {name}! You\'ll be assigned to room {index + 1}!')
+    rooms = [f'Hello, {name}! You\'ll be assigned to room {index + 1}!' for index, name in enumerate(names)]
     return rooms
 
 def printer(names):
